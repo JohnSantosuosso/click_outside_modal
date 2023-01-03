a979d9f54cc865180386fa4c0b29dc91fd6156bc
John Santosuosso <john.santosuosso1@gmail.com>
2023-01-03 08:49:29 -0500

    add modal

---
+++ click-outside.js
@@ -0,0 +1,24 @@
+const cardButtons = document.querySelectorAll('.card button');
+const modalInner = document.querySelector('.modal-inner');
+const modalOuter = document.querySelector('.modal-outer');
+
+
+function handleCardButtonClick(e) {
+  const button = e.currentTarget;
+  const card = button.closest('.card'); //like querySelector all for closest class
+  //Grab image source
+  const imgSrc = card.querySelector('img').src;
+  const desc = card.dataset.description; //because we are using data
+  const name = card.querySelector('h2').textContent;
+  //populate modal with new info
+  modalInner.innerHTML = `
+  <img src="${imgSrc.replace('200', '600')}"
+  alt="${name}"/>
+  <p>${desc}</p>
+  `;
+  //show modal
+  modalInner.outerHTML.classList.add('open');
+}
+
+//have to use forEach because we have multiple cards
+cardButtons.forEach(button => button.addEventListener('click', handleCardButtonClick);
